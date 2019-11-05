cae08d8168cfedda5dcfd48d16c1a605f95adc2e
Alice Pote <alice.writes.wrongs@gmail.com>
2019-11-05 13:42:24 -0500

    Updates

---
+++ precache-manifest.abc344a92c9adf177929bf5fd1a76b7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a0b073abda8cdfc7356332588dddae",
+    "url": "/parallel-texts/index.html"
+  },
+  {
+    "revision": "3a8a607ae7da097cf5b7",
+    "url": "/parallel-texts/static/css/main.e4da3b8d.chunk.css"
+  },
+  {
+    "revision": "6b166f6f838795e7f933",
+    "url": "/parallel-texts/static/js/2.2125cc0e.chunk.js"
+  },
+  {
+    "revision": "3a8a607ae7da097cf5b7",
+    "url": "/parallel-texts/static/js/main.a3f6d52a.chunk.js"
+  },
+  {
+    "revision": "b44a3a83fc35e89c2d12",
+    "url": "/parallel-texts/static/js/runtime~main.3fa88e3f.js"
+  }
+]);
